680089dd7e7e2384fa3b4ffe5d089671d9d57554
Nico Devriendt <nico.devriendt@sint-rembert.be>
2025-02-06 10:36:15 +0100

    Add project files.

---
+++ OrakeltjeDelphi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OrakeltjeDelphi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
